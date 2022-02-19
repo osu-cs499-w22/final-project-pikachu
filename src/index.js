972adb3445725d8a1f42690ef3a8a62f729cf26e
efmmoncada <emmanuel.moncada27@gmail.com>
2022-02-18 16:23:02 -0800

    added initial dependencies and basic global styles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Global, css, jsx } from "@emotion/react";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const globalStyles = css`
+  @import "open-props/style";
+
+  body {
+    margin: 0;
+    font-family: sans-serif; // TODO: Change font
+    box-sizing: border-box;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
