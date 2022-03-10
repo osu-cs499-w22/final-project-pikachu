239340196cfa8bab36e7c82bf2d90c6706322a28
efmmoncada <emmanuel.moncada27@gmail.com>
2022-03-10 14:54:19 -0800

    fix copile error with missing import

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import arcade from "./fonts/Arcade.ttf";
 import { Global, css, jsx } from "@emotion/react";
 
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const globalStyles = css`
   @font-face {
@@ -36,4 +35,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
