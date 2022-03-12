796e41a13d7c22188560c9e056d8b3541b07c503
efmmoncada <emmanuel.moncada27@gmail.com>
2022-03-12 13:50:44 -0800

    simplify and refactor

--- src/hooks/useDamageRelation.js
+++ src/hooks/useDamageRelation.js
@@ -1,31 +1,23 @@
 import { React, useState, useEffect } from "react";
 
-export function useDamageRelations(type1) {
-  //inputs type 1 state and type 2 state
-  const [type1Holder, setType1Holder] = useState("");
-
+export function useDamageRelations(type) {
   //output damage modifiers and types states here;
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
 
   //error and loading states here
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const type1Lower = type1Holder.toLowerCase();
-
-  setType1Holder(type1);
-  setType2Holder(type2);
-
-  const getTypeMatchupOffense = async (type1Check) => {
-    if (!type1Check) {
-      setData([]);
+  const getDamageRelations = async (type) => {
+    if (!type) {
+      setData({});
       return;
     }
     setError(false);
     setLoading(true);
 
     try {
-      const url = `https://pokeapi.co/api/v2/type/${type1Lower}/`;
+      const url = `https://pokeapi.co/api/v2/type/${type}/`;
       const response = await fetch(url);
       const responseJson = await response.json();
       if (responseJson["cod"] === "404" || responseJson["cod"] === "401") {
@@ -40,18 +32,17 @@ export function useDamageRelations(type1) {
       }
     } catch (e) {
       if (e instanceof DOMException) {
-        console.log("==HTTP request cancelled");
+        console.log("== HTTP request cancelled");
       } else {
         throw e;
       }
     }
   };
   useEffect(() => {
-    getTypeMatchupOffense(type1Holder);
-  }, [type1Holder]);
+    getDamageRelations(type);
+  }, [type]);
 
   /* Return works such that it returns a div that contains the matching type and corresponding array. I'm trying to make it
     such that it doesn't return a div but instead returns the necessary information back to the component that calls it*/
-
-  return;
+  return [data, loading, error];
 }
