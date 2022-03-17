af479d6bbbda2c6124b861a5443272532fd88c2c
efmmoncada <emmanuel.moncada27@gmail.com>
2022-03-17 01:51:32 -0700

    add color changing functionallity to settings page

--- src/App.js
+++ src/App.js
@@ -18,88 +18,89 @@ import rightBackgrond from "./pokedexRight.png";
 import Lights from "./components/graphics/Lights";
 import IconBar from "./components/graphics/IconBar";
 import RightIcons from "./components/graphics/rightIcons";
-
-const colors = {
-  red: "none",
-  blue: "hue-rotate(250deg) brightness(100%);",
-  green: "hue-rotate(150deg) brightness(100%);",
-  yellow: "hue-rotate(60deg) brightness(175%)",
-};
-
-const styles = css`
-  background-color: black;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: flex-end;
-
-  .left {
-    background-image: url(${leftBackground});
-    // make this depend on context/state
-    filter: ${colors.red};
-    background-size: 95%;
-    background-repeat: no-repeat;
-    height: 95%;
-    position: relative;
-    left: 1.5em;
-    flex: 1 1 60%;
-
-    .indicator {
-      margin: 2.5em 0 0 2.5em;
-    }
-
-    .lights {
-      position: fixed;
-      left: 15em;
-      top: 5em;
-    }
-
-    .screen {
-      width: 75%;
-      height: 50%;
-      background-color: white;
-      position: relative;
-      top: 5%;
-      left: 8%;
-      border: 5px solid black;
-    }
-
-    .icons {
-      position: relative;
-      top: 10%;
-      margin: 0 auto;
-      width: 90%;
-
-      display: flex;
-    }
-  }
-
-  .right {
-    background-image: url(${rightBackgrond});
-    // make this depend on context/state
-    filter: ${colors.red};
-    background-size: 95%;
-    background-repeat: no-repeat;
-    height: 85%;
-    position: relative;
-    right: 3.5em;
-    flex: 1 1 40%;
-
-    .navbar {
-      position: relative;
-      top: 20%;
-      left: 10%;
-    }
-
-    .right-icon-bar {
-      position: relative;
-      top: 15em;
-      left: 3em;
-    }
-  }
-`;
+import { useState } from "react";
 
 function App() {
+  const [selectedColor, setSelectedColor] = useState("red");
+
+  const colors = {
+    red: "none",
+    blue: "hue-rotate(250deg) brightness(100%);",
+    green: "hue-rotate(150deg) brightness(100%);",
+    yellow: "hue-rotate(60deg) brightness(175%)",
+  };
+
+  const styles = css`
+    background-color: black;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+
+    .left {
+      background-image: url(${leftBackground});
+      filter: ${colors[`${selectedColor}`]};
+      background-size: 95%;
+      background-repeat: no-repeat;
+      height: 95%;
+      position: relative;
+      left: 1.5em;
+      flex: 1 1 60%;
+
+      .indicator {
+        margin: 2.5em 0 0 2.5em;
+      }
+
+      .lights {
+        position: absolute;
+        left: 13em;
+        top: 3.5em;
+      }
+
+      .screen {
+        width: 75%;
+        height: 50%;
+        background-color: white;
+        position: relative;
+        top: 5%;
+        left: 8%;
+        border: 5px solid black;
+      }
+
+      .icons {
+        position: relative;
+        top: 10%;
+        margin: 0 auto;
+        width: 90%;
+
+        display: flex;
+      }
+    }
+
+    .right {
+      background-image: url(${rightBackgrond});
+      filter: ${colors[`${selectedColor}`]};
+      background-size: 95%;
+      background-repeat: no-repeat;
+      height: 85%;
+      position: relative;
+      right: 3.5em;
+      flex: 1 1 40%;
+
+      .navbar {
+        position: relative;
+        top: 20%;
+        left: 10%;
+      }
+
+      .right-icon-bar {
+        position: relative;
+        top: 15em;
+        left: 3em;
+      }
+    }
+  `;
+
   return (
     <div css={styles}>
       <div className='left'>
@@ -111,7 +112,10 @@ function App() {
           <Route path='/favorites' element={<FavoritesPage />} />
           <Route path='/nuzlocke' element={<NuzlockePage />} />
           <Route path='/growth' element={<GrowthPage />} />
-          <Route path='/settings' element={<SettingsPage />} />
+          <Route
+            path='/settings'
+            element={<SettingsPage changeColor={setSelectedColor} />}
+          />
         </Routes>
         <div className='icons'>
           <IconBar />
--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const SettingsPage = () => {
-  return <div className='screen'>Settings Page</div>;
+const SettingsPage = ({ changeColor }) => {
+  return (
+    <div className='screen'>
+      <select
+        name='color'
+        id='color'
+        onChange={(e) => changeColor(e.target.value)}
+      >
+        <option value='red'>Red</option>
+        <option value='blue'>Blue</option>
+        <option value='green'>Green</option>
+        <option value='yellow'>Yellow</option>
+      </select>
+    </div>
+  );
 };
 
 export default SettingsPage;
