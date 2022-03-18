1b397666e3a1ff2f76bff6ec9c9754c6a89757e5
tan-justin <tanju@oregonstate.edu>
2022-03-18 11:34:47 -0700

    FormMoves.js and MovesPage.js change

--- src/pages/MovesPage.js
+++ src/pages/MovesPage.js
@@ -1,7 +1,38 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+
+import React, {useState} from "react";
+import { css, jsx } from "@emotion/react";
+import { FormMoves } from "../components/FormMoves";
+import { useDamageRelations } from "../hooks/useDamageRelation";
+import { usePokemon } from "../hooks/usePokemon";
+
 
 const MovesPage = () => {
-  return <div className='screen'>Moves Page</div>;
-};
 
+  const [pokemon, setPokemon] = useState('');
+  const [version, setVersion] = useState('');
+  const [dataPokemon, loading, error] = usePokemon(pokemon.toLowerCase());
+  console.log(dataPokemon);
+  /*if(dataPokemon.type){
+      console.log(dataPokemon.type[0].type.name)
+  }*/
+ // const [damageData1, loadingDamage1, errorDamage1] = useDamageRelations(dataPokemon.type[0].type.name);
+
+  
+
+  return(
+    <div className='screen'>
+      <FormMoves pokemon={pokemon} setPokemon={setPokemon} version={version} setVersion={setVersion}/>
+      {(dataPokemon) ? 
+        <div></div> 
+        
+        
+        
+        : null
+
+      }
+    </div>
+
+  )
+}
 export default MovesPage;
